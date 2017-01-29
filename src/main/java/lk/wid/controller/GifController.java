3eec3a6ea979cb1a869c45cabcc395dc8558932f
Lukasz Widawski <lk.widawski@gmail.com>
2017-01-29 12:40:40 +0100

    gif view by name, view all gifs

--- src/main/java/lk/wid/controller/GifController.java
+++ src/main/java/lk/wid/controller/GifController.java
@@ -1,14 +1,31 @@
 package lk.wid.controller;
 
+import lk.wid.repository.GifRepository;
+import lk.wid.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 public class GifController {
-    @ResponseBody
+
+    @Autowired
+    private GifRepository gifRepository;
+
     @GetMapping("/")
-    public String listGifs() {
-        return "Hello World";
+    public String listGifs(ModelMap modelMap) {
+        modelMap.addAttribute("gifs", gifRepository.getAllGifs());
+        return "home";
+    }
+
+    @GetMapping("/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap) {
+        Gif gif = gifRepository.findByName(name);
+        modelMap.addAttribute("gif", gif);
+        return "gif-details";
     }
 }
---
+++ src/main/java/lk/wid/repository/GifRepository.java
@@ -0,0 +1,35 @@
+package lk.wid.repository;
+
+import lk.wid.model.Gif;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+public class GifRepository {
+    private static final List<Gif> ALL_GIFS = Arrays.asList(
+            new Gif("android-explosion", "Chris Ramacciotti", false),
+            new Gif("ben-and-mike", "Ben Jakuben", true),
+            new Gif("book-dominos", "Craig Dennis", false),
+            new Gif("compiler-bot", "Ada Lovelace", true),
+            new Gif("cowboy-coder", "Grace Hopper", false),
+            new Gif("infinite-andrew", "Marissa Mayer", true)
+    );
+
+    public GifRepository() {
+    }
+
+    public List<Gif> getAllGifs() {
+        return ALL_GIFS;
+    }
+
+    public Gif findByName(String name) {
+        for (Gif gif : ALL_GIFS) {
+            if (gif.getName().equals(name)) {
+                return gif;
+            }
+        }
+        return null;
+    }
+}
